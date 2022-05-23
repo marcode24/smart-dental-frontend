966048dfdedc35cba8c977a920545b970b595878
marco cruz <marco24cruz08@gmail.com>
2022-05-23 12:08:33 -0500

    :sparkles: validate price service with regex expression added

--- src/app/core/utils/regex.util.ts
+++ src/app/core/utils/regex.util.ts
@@ -2,7 +2,9 @@ export class RegexClass {
   public static readonly ONLY_TEXT = /^[a-zA-Z\ñÀ-ú ]{2,20}$/;
   public static readonly PHONE_NUMBER = /^[0-9]{10,12}$/;
   public static readonly CP = /^[0-9]{5}$/;
-  public static readonly STREET = /^[a-zA-Z0-9\ñÀ-ú ]{2,50}/;
-  public static readonly USER_NAME = /^[a-zA-Z0-9]{8,24}/;
-  public static readonly PASSWORD = /^[a-zA-Z0-9]{8,}/;
+  public static readonly STREET = /^[a-zA-Z0-9\ñÀ-ú ]{2,50}$/;
+  public static readonly USER_NAME = /^[a-zA-Z0-9]{8,24}$/;
+  public static readonly PASSWORD = /^[a-zA-Z0-9]{8,}$/;
+  public static readonly PRICE = /^[0-9]{0,}(\.[0-9]{0,2})?$/;
+  public static readonly TEXT_SERVICE = /^[a-zA-Z\ñÀ-ú ]{2,120}$/;
 }
--- src/app/core/utils/validation-date-birth.util.ts
+++ src/app/core/utils/validation-date-birth.util.ts
@@ -3,16 +3,18 @@ import { AbstractControl, ValidatorFn } from "@angular/forms";
 export default class ValidationDateBirth {
   static validate(controlDate: string): ValidatorFn {
     return (controls: AbstractControl) => {
-      const control = controls.get(controlDate)?.value.split('-');
-      const dateSelected = new Date(control[0], control[1] - 1, control[2]).setHours(0,0,0,0);
-      const today = new Date().setHours(0,0,0,0);
-
-      if(dateSelected >= today) {
-        controls.get(controlDate)?.setErrors({ isMax: true });
-        return { isMax: true };
-      } else {
-        return null;
-      }
+      if(controls.get(controlDate)?.value) {
+        const control = controls.get(controlDate)?.value.split('-');
+        const dateSelected = new Date(control[0], control[1] - 1, control[2]).setHours(0,0,0,0);
+        const today = new Date().setHours(0,0,0,0);
+        if(dateSelected >= today) {
+          controls.get(controlDate)?.setErrors({ isMax: true });
+          return { isMax: true };
+        } else {
+          return null;
+        }
+      };
+      return null;
     }
   }
 }
