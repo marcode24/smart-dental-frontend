6867051e49303e5557ec31102662c3d9ae09c863
marco <marco24cruz08@gmail.com>
2023-03-09 13:13:52 -0600

    :recycle: refact tooth map features

--- src/app/features/patients/components/odontogram/odontogram.component.ts
+++ src/app/features/patients/components/odontogram/odontogram.component.ts
@@ -13,8 +13,8 @@ import { Tooth } from '@models/tooth.model';
 export class OdontogramComponent implements OnChanges {
 
   @Input() teeth: Tooth[];
-  public up = [18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28];
-  public down = [48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38];
+  public upSequence = [18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28];
+  public downSequence = [48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38];
 
   public upTeeth: Tooth[];
   public downTeeth: Tooth[];
@@ -34,91 +34,33 @@ export class OdontogramComponent implements OnChanges {
 
   getServicesActive(tooth_number: number, position: 'up' | 'down') {
     this.toothTemp = (position === 'up')
-      ? this.upTeeth.find(t => t.tooth_number === tooth_number):
-      this.downTeeth.find(t => t.tooth_number === tooth_number);
+      ? this.upTeeth.find(t => t.tooth_number === tooth_number)
+      : this.downTeeth.find(t => t.tooth_number === tooth_number);
     this.serviceOfferServices.getServicesActive(true);
   }
 
   setData(teeth: Tooth[]) {
     this.loadDataTeeth();
     teeth.forEach((tooth: Tooth) => {
-      const belongsUp = this.up.includes(tooth.tooth_number);
-      const belongsDown = this.down.includes(tooth.tooth_number);
+      const belongsUp = this.upSequence.includes(tooth.tooth_number);
       if(belongsUp) {
-        this.upTeeth.map(t => {
-          if (t.tooth_number === tooth.tooth_number) {
-            const {
-              distal, ligual, mesial, oclusal,
-              vestibular, record, id_tooth }
-            = tooth;
-            t.id_tooth = id_tooth;
-            t.distal = distal;
-            t.ligual = ligual;
-            t.mesial = mesial;
-            t.oclusal = oclusal;
-            t.vestibular= vestibular;
-            t.record = record;
-            t.color = record?.service?.color;
-          }
+        this.upTeeth = this.upTeeth.map(upTooth => {
+          return upTooth.tooth_number === tooth.tooth_number
+            ? upTooth = { ...tooth, color: tooth.record?.service?.color }
+            : upTooth;
         });
-      }
-      if(belongsDown) {
-        this.downTeeth.map(t => {
-          if (t.tooth_number === tooth.tooth_number) {
-            const {
-              distal, ligual, mesial, oclusal,
-              vestibular, record, id_tooth
-            } = tooth;
-            t.id_tooth = id_tooth;
-            t.distal = distal;
-            t.ligual = ligual;
-            t.mesial = mesial;
-            t.oclusal = oclusal;
-            t.vestibular= vestibular;
-            t.record = record;
-            t.color = record?.service?.color;
-          }
+      } else {
+        this.downTeeth = this.downTeeth.map(downTooth => {
+          return downTooth.tooth_number === tooth.tooth_number
+            ? downTooth = { ...tooth, color: tooth.record?.service?.color  }
+            : downTooth;
         });
       }
     });
   }
 
   loadDataTeeth() {
-    this.downTeeth = [
-      { tooth_number: 48 },
-      { tooth_number: 47 },
-      { tooth_number: 46 },
-      { tooth_number: 45 },
-      { tooth_number: 44 },
-      { tooth_number: 43 },
-      { tooth_number: 42 },
-      { tooth_number: 41 },
-      { tooth_number: 31 },
-      { tooth_number: 32 },
-      { tooth_number: 33 },
-      { tooth_number: 34 },
-      { tooth_number: 35 },
-      { tooth_number: 36 },
-      { tooth_number: 37 },
-      { tooth_number: 38 }
-    ];
-    this.upTeeth = [
-      { tooth_number: 18 },
-      { tooth_number: 17 },
-      { tooth_number: 16 },
-      { tooth_number: 15 },
-      { tooth_number: 14 },
-      { tooth_number: 13 },
-      { tooth_number: 12 },
-      { tooth_number: 11 },
-      { tooth_number: 21 },
-      { tooth_number: 22 },
-      { tooth_number: 23 },
-      { tooth_number: 24 },
-      { tooth_number: 25 },
-      { tooth_number: 26 },
-      { tooth_number: 27 },
-      { tooth_number: 28 }
-    ];
+    this.downTeeth = this.downSequence.map((num) => ({ tooth_number: num }));
+    this.upTeeth = this.upSequence.map((num) => ({ tooth_number: num }));
   }
 }
