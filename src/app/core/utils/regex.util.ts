d600e9da5737bdc35c8fcc803feacb6e49d8fff8
marco <marco24cruz08@gmail.com>
2023-06-15 13:34:12 -0600

    :pencil2: fix phone number regex

--- src/app/core/utils/regex.util.ts
+++ src/app/core/utils/regex.util.ts
@@ -1,6 +1,6 @@
 export class RegexClass {
   public static readonly ONLY_TEXT = /^[a-zA-ZñÀ-ú ]{2,20}$/;
-  public static readonly PHONE_NUMBER = /^[0-9]{10,12}$/;
+  public static readonly PHONE_NUMBER = /^(?![0])[0-9]{10,12}$/;
   public static readonly CP = /^[0-9]{5}$/;
   public static readonly STREET = /^[a-zA-Z0-9ñÀ-ú ]{2,50}$/;
   public static readonly USER_NAME = /^[a-zA-Z0-9]{8,24}$/;
@@ -8,5 +8,5 @@ export class RegexClass {
   public static readonly PRICE = /^[0-9]{0,}(\.[0-9]{0,2})?$/;
   public static readonly TEXT_SERVICE = /^[a-zA-ZñÀ-ú ]{2,120}$/;
   public static readonly NUMBER = /^[0-9]{1,6}$/;
-  public static readonly EMAIL = /^^(?![.\-_])[A-Za-z0-9._-]+@([a-z]+.)+[a-z]{2,4}$/;
+  public static readonly EMAIL = /^(?![.\-_])[A-Za-z0-9._-]+@([a-z]+.)+[a-z]{2,4}$/;
 }
