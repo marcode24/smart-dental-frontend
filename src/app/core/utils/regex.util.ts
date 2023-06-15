b6095bc85095cf18868b89dbb21053891d608bde
marco <marco24cruz08@gmail.com>
2023-06-15 13:43:21 -0600

    :pencil2: fix cp regex

--- src/app/core/utils/regex.util.ts
+++ src/app/core/utils/regex.util.ts
@@ -1,7 +1,7 @@
 export class RegexClass {
   public static readonly ONLY_TEXT = /^[a-zA-ZñÀ-ú ]{2,20}$/;
   public static readonly PHONE_NUMBER = /^(?![0])[0-9]{10,12}$/;
-  public static readonly CP = /^[0-9]{5}$/;
+  public static readonly CP = /^(?!00000)\d{5}$/;
   public static readonly STREET = /^[a-zA-Z0-9ñÀ-ú ]{2,50}$/;
   public static readonly USER_NAME = /^[a-zA-Z0-9]{8,24}$/;
   public static readonly PASSWORD = /^[a-zA-Z0-9]{8,}$/;
